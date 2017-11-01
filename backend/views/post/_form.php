0aeef3a6f86627a35908971f44ee399c263d759a
zhaoxiaojian <380406228@qq.com>
2017-11-01 17:51:34 +0800

    get model

--- backend/views/post/_form.php
+++ backend/views/post/_form.php
@@ -2,6 +2,8 @@
 
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
+use common\models\Poststatus;
+use yii\helpers\ArrayHelper;
 
 /* @var $this yii\web\View */
 /* @var $model common\models\Post */
@@ -18,7 +20,17 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'tags')->textarea(['rows' => 6]) ?>
 
+    <!--
     <?= $form->field($model, 'status')->textInput() ?>
+    -->
+    <!-- hardcode硬编码
+    <?= $form->field($model, 'status')->dropDownList([1=>'草稿', 2=>'已发布'], ['prompt'=>'请选择状态']); ?>
+    -->
+    <?php
+    $psObjs = Poststatus::find()->all();
+    $allStatus = ArrayHelper::map($psObjs, 'id', 'name');
+    ?>
+    <?= $form->field($model, 'status')->dropDownList($allStatus, ['prompt'=>'请选择状态']); ?>
 
     <?= $form->field($model, 'create_time')->textInput() ?>
 
@@ -27,7 +39,7 @@ use yii\widgets\ActiveForm;
     <?= $form->field($model, 'author_id')->textInput() ?>
 
     <div class="form-group">
-        <?= Html::submitButton($model->isNewRecord ? 'Create' : 'Update', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
+        <?= Html::submitButton($model->isNewRecord ? '新建' : '更新', ['class' => $model->isNewRecord ? 'btn btn-success' : 'btn btn-primary']) ?>
     </div>
 
     <?php ActiveForm::end(); ?>
